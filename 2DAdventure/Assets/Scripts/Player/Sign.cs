366507e3aa751b9669dfcfede929e71f0c341c66
Bayu鲅魚 <yujiaybrera@gmail.com>
2025-08-09 18:39:42 +0800

    手柄支持

--- 2DAdventure/Assets/Scripts/Player/Sign.cs
+++ 2DAdventure/Assets/Scripts/Player/Sign.cs
@@ -1,18 +1,74 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.DualShock;
 
 public class Sign : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInputControl playerInput;//拿到玩家的输入
+    private Animator anim;
+
+    public Transform palyerTrans;
+    public GameObject signSprite;
+    private bool canPress;
+
+    private void Awake()
     {
-        
+        //anim = GetComponentInChildren<Animator>();
+        anim = signSprite.GetComponent<Animator>();
+
+        playerInput = new PlayerInputControl();
+        playerInput.Enable();
+
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        InputSystem.onActionChange += OnActionChange;
+    }
+
     void Update()
     {
-        
+        signSprite.GetComponent<SpriteRenderer>().enabled = canPress;
+        signSprite.transform.localScale = palyerTrans.localScale;
+    }
+
+    private void OnActionChange(object obj, InputActionChange actionChange)//unity自己的按钮变换是注册的方法
+    {
+        if (actionChange == InputActionChange.ActionStarted)
+        {
+            Debug.Log(((InputAction)obj).activeControl.device);
+
+            var d = ((InputAction)obj).activeControl.device;
+
+            switch (d.device)
+            {
+                case Keyboard:
+                    anim.Play("keyboard");
+                    break;
+
+                case DualShockGamepad:
+                    anim.Play("ps");
+                    break;
+            }
+
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            canPress = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        canPress = false;
     }
 }
