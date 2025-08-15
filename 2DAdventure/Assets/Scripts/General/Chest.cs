5f19b60cc3872df94a9ea9c0b27641b9c84a31f7
Bayu鲅魚 <yujiaybrera@gmail.com>
2025-08-15 15:35:01 +0800

    开宝箱加血

--- 2DAdventure/Assets/Scripts/General/Chest.cs
+++ 2DAdventure/Assets/Scripts/General/Chest.cs
@@ -8,6 +8,7 @@ public class Chest : MonoBehaviour, IInteractable
     public Sprite openSprite;
     public Sprite closeSprite;
     public bool isDone;
+    public int healAmount = 10;
 
     private void Awake()
     {
@@ -31,6 +32,32 @@ public class Chest : MonoBehaviour, IInteractable
         spriteRenderer.sprite = openSprite;
         isDone = true;
         this.gameObject.tag = "Untagged";
+
+        // 给予玩家加血（最小改动方案）
+        Character character = null;
+        var playerGO = GameObject.FindGameObjectWithTag("Player");
+        if (playerGO != null)
+        {
+            playerGO.TryGetComponent<Character>(out character);
+        }
+        if (character == null)
+        {
+            // 兜底：场景里找一个带有 PlayerController 的对象并取其 Character
+            var playerController = FindObjectOfType<PlayerController>();
+            if (playerController != null)
+            {
+                character = playerController.GetComponent<Character>();
+            }
+        }
+        if (character != null)
+        {
+            character.currentHealth = Mathf.Min(character.currentHealth + healAmount, character.maxHealth);
+            character.OnHealthChange?.Invoke(character);
+        }
+        else
+        {
+            Debug.LogWarning("未找到玩家的 Character 组件，无法执行加血。");
+        }
     }
   
 }
